309cdffbda6a3fe6ffd08711b28cb558d1784282
Lijie Zhou <lijie@mail.sfsu.edu>
2017-01-27 13:54:07 -0800

    Add CSRF token

--- private/csrf_functions.php
+++ private/csrf_functions.php
@@ -9,7 +9,14 @@
   // Returns HTML for a hidden form input with a CSRF token as the value
   function csrf_token_tag() {
     // TODO needs to set a token and put in an HTML tag
-    return '<input type="TODO" name="TODO" value="TODO" />';
+    if(!isset($_SESSION['csrf_token'])){
+      $token = csrf_token();
+      $_SESSION['csrf_token'] = $token;
+      $_SESSION['token_time'] = time();
+    }else{
+      $token = $_SESSION['csrf_token'];
+    }
+    return '<input type="hidden" name="token"  value="' .h($token) . '"/>';
   }
 
   // Returns true if form token matches session token, false if not.
@@ -23,7 +30,18 @@
   // by comparing it to the time a token was last generated.
   function csrf_token_is_recent() {
     // TODO add code to determine if csrf token is recent
-    return true;
+    $recent_limit = 600; //set 10minite limit
+    return ((time()-$_SESSION['token_time']) < $recent_limit);
+
+  }
+
+  function exit_if_csrf_attack(){
+    if(!csrf_token_is_valid() || !csrf_token_is_recent()){
+      exit("Error: CSRF token doesn't match!");
+    }
+    else{
+      //Do nothing!
+    }
   }
 
 ?>
